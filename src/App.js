e0743b935fdccaa7f00fe8efa7546247e731ea0b
kietlac99 <bucu130599@gmail.com>
2024-03-22 01:03:02 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
