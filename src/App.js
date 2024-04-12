2ae94df9e22466b45ac664b3878c63bc897cfc7c
kietlac99 <bucu130599@gmail.com>
2024-04-12 10:04:28 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import Payment from "./components/cart/Payment";
 // Auth or User Imports
 import Login from './components/user/Login';
 import Register from './components/user/Register';
+import RegisterConfirm from './components/user/RegisterConfirm';
 import Profile from './components/user/Profile';
 import UpdateProfile from "./components/user/UpdateProfile";
 import UpdatePassword from "./components/user/UpdatePassword";
@@ -95,6 +96,7 @@ function App() {
 
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
+          <Route path='/register/confirm/:email' component={RegisterConfirm} />
           <Route path="/password/forgot" component={ForgotPassword} exact />
           <Route path="/password/reset/:token" component={NewPassword} exact />
           <ProtectedRoute path="/me" component={Profile} exact />
